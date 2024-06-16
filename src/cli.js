10f7f747666d3b6c9f0495b8a5fc6ec1fab3b476
Victoria-V-V <victoriavolcheck@yandex.ru>
2024-06-17 02:38:28 +0700

    feat: add greeting

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+
+    const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have you name? ');
+    console.log(`Hello, ${userName}!`);
+};
+
+export default greeting;

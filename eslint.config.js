ed35ca4dfbda63c72dcc6fb7f3011cd0cd0829fa
Victoria-V-V <victoriavolcheck@yandex.ru>
2024-06-18 03:01:16 +0700

    style: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,72 @@
+import globals from "globals";
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import importPlugin from 'eslint-plugin-import';
+import js from "@eslint/js";
+// import { fixupPluginRules } from "@eslint/compat";
+// import example from "eslint-plugin-example";
+
+const __filename = fileURLToPath(
+  import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended
+});
+
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      import: importPlugin,
+      // example: fixupPluginRules(example)
+    },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ["**/node_modules/", ".git/", 'eslint.config.js'],
+  },
+  ...compat.extends('airbnb-base'),
+  {
+    rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/no-amd': 'off', // added
+      'import/newline-after-import': 'off', //added
+      'import/no-mutable-exports': 'off', //added
+    },
+  },
+];
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
 import readlineSync from 'readline-sync';
-
 const greeting = () => {
-
-    const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have you name? ');
-    console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have you name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default greeting;

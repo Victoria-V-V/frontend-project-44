0f176e90285580d3bbbbe7c9461bc5b0a8916a87
Victoria-V-V <victoriavolcheck@yandex.ru>
2024-06-24 05:58:01 +0700

    fix: split greeting into two lines

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,8 @@ import readlineSync from 'readline-sync';
 let userName;
 
 const greeting = () => {
-  userName = readlineSync.question('Welcome to the Brain Games!\nMay I have you name? ');
+  console.log('Welcome to the Brain Games!');
+  userName = readlineSync.question('May I have you name? ');
   console.log(`Hello, ${userName}!`);
 };
 

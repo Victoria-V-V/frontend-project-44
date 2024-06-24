e7202284b4ebdc55a9d6094426ef9ee77db037c4
Victoria-V-V <victoriavolcheck@yandex.ru>
2024-06-24 09:03:00 +0700

    refactor: update numbers range for brain-even

--- src/games/brain-even-lib.js
+++ src/games/brain-even-lib.js
@@ -6,7 +6,7 @@ const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const getCurrentQuestion = () => {
-  const number = getRandomInt(0, 100);
+  const number = getRandomInt(0, 200);
 
   const correctAnswer = isEven(number) ? 'yes' : 'no';
   const question = `${number}`;

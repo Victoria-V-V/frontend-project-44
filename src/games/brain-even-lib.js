faf8676315f1d4274b462de29380bc5dedfcd43c
Victoria-V-V <victoriavolcheck@yandex.ru>
2024-06-24 08:05:25 +0700

    refactor: update brain-even

--- src/games/brain-even-lib.js
+++ src/games/brain-even-lib.js
@@ -3,15 +3,12 @@ import getRandomInt from '../utils.js';
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => {
-  const result = (number % 2 === 0) ? 'yes' : 'no';
-  return result;
-};
+const isEven = (number) => number % 2 === 0;
 
 const getCurrentQuestion = () => {
   const number = getRandomInt(0, 100);
 
-  const correctAnswer = isEven(number);
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
   const question = `${number}`;
   return [correctAnswer, question];
 };

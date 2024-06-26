584dafddbf475cdaf9545c372efc2fabbc8e3abd
Victoria-V-V <victoriavolcheck@yandex.ru>
2024-06-26 11:53:39 +0700

    refactor(index.js): add variable maxQuestionsCount

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ const launchGame = (gameRules, getCurrentQuestion) => {
   console.log(gameRules);
 
   let currentAnswer = 0;
+  const maxQuestionsCount = 3;
 
-  while (currentAnswer < 3) {
+  while (currentAnswer < maxQuestionsCount) {
     const [correctAnswer, question] = getCurrentQuestion();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
@@ -15,7 +16,7 @@ const launchGame = (gameRules, getCurrentQuestion) => {
     if (String(correctAnswer) === userAnswer) {
       currentAnswer += 1;
       console.log('Correct!');
-      if (currentAnswer === 3) {
+      if (currentAnswer === maxQuestionsCount) {
         console.log(`Congratulations, ${userName}!`);
       }
     } else {
